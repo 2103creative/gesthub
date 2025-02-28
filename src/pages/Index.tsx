d8cfa0b84b4b08fe7acb46a1175a135e0aa0c622
2103creative <2103creative@gmail.com>
2025-02-28 15:27:08 +0000

    rev001

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -19,7 +19,7 @@ const MainMenu = () => {
     <Link 
       key={to}
       to={to}
-      className="block w-[200px] h-[40px] leading-[40px] mx-auto bg-[#ea384c] hover:bg-red-600 transition-colors duration-200 rounded-lg shadow-sm"
+      className="block w-[200px] h-[40px] leading-[40px] mx-auto bg-eink-black hover:bg-eink-gray transition-colors duration-200 rounded-lg shadow-sm"
     >
       <span className="text-[14px] font-bold text-white uppercase font-quicksand">{label}</span>
     </Link>
@@ -29,7 +29,7 @@ const MainMenu = () => {
     <div className="min-h-screen flex flex-col justify-between bg-eink-white text-eink-black animate-fadeIn">
       <div className="flex-grow flex flex-col justify-center">
         <div className="max-w-[80%] w-full mx-auto px-3 py-6">
-          <h1 className="text-[18px] font-quicksand font-light text-center mb-6 uppercase">
+          <h1 className="text-[18px] font-quicksand font-light text-center mb-10 uppercase">
             GestHub
           </h1>
           
@@ -39,8 +39,8 @@ const MainMenu = () => {
         </div>
       </div>
       
-      <footer className="w-full py-3 text-xs text-eink-gray text-center uppercase">
-        © 2025 - DESENVOLVIDO POR 2103 CREATIVE - DESDE 2024
+      <footer className="w-full py-10 text-xs text-eink-gray text-center uppercase">
+        © 2103 CREATIVE - 2025
       </footer>
     </div>
   );
